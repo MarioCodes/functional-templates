abee3bbd78dfb9eb45cd87a3adaae454f6140549
Mario Codes <mariocodes1993@gmail.com>
2019-04-11 13:05:36 +0200

    java-kotlin: added multi template

---
+++ java-kotlin/src/main/java/es/msanchez/templates/java/kotlin/beans/JavaBean.java
@@ -0,0 +1,10 @@
+package es.msanchez.templates.java.kotlin.beans;
+
+public class JavaBean {
+
+  @Override
+  public String toString() {
+    return "This is a Java Bean";
+  }
+
+}
---
+++ java-kotlin/src/main/kotlin/es/msanchez/templates/java/kotlin/beans/KotlinBean.kt
@@ -0,0 +1,9 @@
+package es.msanchez.templates.java.kotlin.beans
+
+class KotlinBean {
+
+    override fun toString(): String {
+        return "This is a Kotlin Bean"
+    }
+
+}
