255c1bbe77fdd566ecdee1b111f40eab0bec0049
Mario Codes <mariocodes1993@gmail.com>
2019-04-11 12:55:29 +0200

    all: made all poms similar

--- java-spring-vertx/src/main/java/es/msanchez/templates/java/full/config/SpringConfig.java
+++ java-spring-vertx/src/main/java/es/msanchez/templates/java/full/config/SpringConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration @ComponentScan(basePackages = { "es.msanchez.templates.complete.**.*" })
+@Configuration @ComponentScan(basePackages = { "es.msanchez.templates.java.full.**.*" })
 public class SpringConfig {
 
   /**
--- java-spring/src/main/java/es/msanchez/templates/java/spring/config/SpringConfig.java
+++ java-spring/src/main/java/es/msanchez/templates/java/spring/config/SpringConfig.java
@@ -3,6 +3,6 @@ package es.msanchez.templates.java.spring.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration @ComponentScan(basePackages = { "es.msanchez.templates.spring.**.*" }) public class SpringConfig {
+@Configuration @ComponentScan(basePackages = { "es.msanchez.templates.java.spring.**.*" }) public class SpringConfig {
 
 }
