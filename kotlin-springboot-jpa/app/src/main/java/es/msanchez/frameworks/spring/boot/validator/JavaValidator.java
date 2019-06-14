2ee9c91fd6f04eaa9fd1bd0655993500ea7ee603
Mario Codes <mariocodes1993@gmail.com>
2019-06-14 14:45:48 +0200

    kotlin-jpa: copied kotlin springboot template

---
+++ kotlin-springboot-jpa/app/src/main/java/es/msanchez/frameworks/spring/boot/validator/JavaValidator.java
@@ -0,0 +1,12 @@
+package es.msanchez.frameworks.spring.boot.validator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JavaValidator {
+
+    public String validate() {
+      return "This is the Java validator";
+    }
+
+}
---
+++ kotlin-springboot-jpa/app/src/main/kotlin/es.msanchez.frameworks.spring.boot/controller/KotlinRestController.kt
@@ -0,0 +1,19 @@
+package es.msanchez.frameworks.spring.boot.controller
+
+import es.msanchez.frameworks.spring.boot.validator.KotlinValidator
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class KotlinRestController {
+
+    @Autowired
+    lateinit var kotlinValidator: KotlinValidator
+
+    @RequestMapping("/kotlin")
+    fun index(): String {
+        return kotlinValidator.validate()
+    }
+
+}
