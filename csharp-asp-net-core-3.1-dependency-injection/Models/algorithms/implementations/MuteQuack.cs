65408b5880918d7336810bd9920058332570b897
Mario Codes <mariocodes1993@gmail.com>
2024-04-04 19:23:32 +0200

    add new basic template - left to fix Program

---
+++ csharp-asp-net-core-3.1-dependency-injection/Models/algorithms/implementations/MuteQuack.cs
@@ -0,0 +1,14 @@
+﻿using template.Models.algorithms.interfaces;
+using System;
+
+namespace template.Models.algorithms.implementations
+{
+    public class MuteQuack : IQuackBehaviour
+    {
+        public void Quack()
+        {
+            Console.WriteLine("<<Silence>>");
+        }
+
+    }
+}
---
+++ csharp-asp-net-core-3.1-dependency-injection/Models/algorithms/interfaces/IQuackBehaviour.cs
@@ -0,0 +1,7 @@
+﻿namespace template.Models.algorithms.interfaces
+{
+    public interface IQuackBehaviour
+    {
+        void Quack();
+    }
+}
