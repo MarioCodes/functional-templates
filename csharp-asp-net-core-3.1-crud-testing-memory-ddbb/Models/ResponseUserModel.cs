92beec72e9bad2d6b17e7fdf024f703c0c74c254
Mario Codes <mariocodes1993@gmail.com>
2024-05-12 11:41:01 +0200

    added new testing project template

---
+++ csharp-asp-net-core-3.1-crud-testing-memory-ddbb/Models/ResponseUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace crud.Models
+{
+    public class ResponseUserModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    public class ResponseListUserModel
+    {
+        public List<ResponseUserModel> Result { get; set; }
+    }
+}
---
+++ csharp-asp-net-core-3.1-crud-testing-memory-ddbb/Startup.cs
@@ -0,0 +1,63 @@
+using crud.Configuration;
+using crud.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace crud
+{
+    public class Startup
+    {
+
+        public IConfiguration _config { get; }
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Service Configuration
+            services.AddControllers();
+            services.AddSwaggerGen();
+
+            // Services
+            services.AddScoped<IUserService, UserService>();
+            
+            // Database
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase(databaseName: "InMemoryTestDatabase"));
+
+            // Custom Configurations
+            services.Configure<UserConfig>(_config.GetSection(UserConfig.Section));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
