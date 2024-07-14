e89375483662303a5373aa911e7fbeb02134bb79
Mario Codes <mariocodes1993@gmail.com>
2024-07-14 20:00:25 +0200

    add new .net 8 project

---
+++ csharp-asp-net-core-8-crud-memory-ddbb/Configuration/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Configuration
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ csharp-asp-net-core-8-crud-memory-ddbb/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is mandatory")]
+        public string Email { get; set; }
+    }
+}
