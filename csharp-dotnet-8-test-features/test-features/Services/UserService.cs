a3adf9563fb0d7ad82e6c405375f8deae8e15261
Mario Codes <mariocodes1993@gmail.com>
2024-08-31 19:27:05 +0200

    add new project for dotnet 8

---
+++ csharp-dotnet-8-test-features/test-features/Services/UserService.cs
@@ -0,0 +1,78 @@
+﻿using testFeatures.Configuration;
+using testFeatures.Models;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testFeatures.Services
+{
+    // implement real database service. 
+    public class UserService(AppDbContext _dbContext, IOptions<UserConfig> userConfig) : IUserService
+    {
+
+        private UserConfig _userConfig => userConfig.Value;
+
+        public async Task<string> GetRegex()
+        {
+            return _userConfig.EmailRegex;
+        }
+
+        public async Task<string> CreateUser(RequestUserModel userModel)
+        {
+            var newId = _dbContext.Users
+                .Select(x => x.Id).Max()+1;
+            User user = await MapRequestUserToUser(userModel, newId);
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return "user created";
+        }
+        private async Task<User> MapRequestUserToUser(RequestUserModel requestUser, int id)
+        {
+            return new User
+            {
+                Id = id,
+                Name = requestUser.Name,
+                Email = requestUser.Email
+            };
+        }
+
+        public async Task DeleteUsers(List<RequestUserModel> userModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ResponseUserModel> GetSpecificUser(RequestUserModel userModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ResponseListUserModel> GetUsers()
+        {
+            var users = _dbContext.Users.ToList();
+            List<ResponseUserModel> responseUsers = users.Select(user => MapUserToResponseUser(user))
+                .ToList();
+
+            return new ResponseListUserModel
+            {
+                Result = responseUsers
+            };
+        }
+
+        private ResponseUserModel MapUserToResponseUser(User user)
+        {
+            var userModel = new ResponseUserModel
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email
+            };
+            return userModel;
+        }
+
+        public async Task<string> UpdateUser(RequestUserModel userModel)
+        {
+            return "user updated";
+        }
+    }
+}
