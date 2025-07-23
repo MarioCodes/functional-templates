147861a185b63aad4f38b24bfffbf3022bcc0685
Mario Codes <mariocodes1993@gmail.com>
2025-07-23 20:54:25 +0200

    add TAP Programming test

---
+++ csharp-dotnet-8-testing-zone/testing-zone/Models/CarModels.cs
@@ -0,0 +1,19 @@
+﻿namespace testingZone.Models
+{
+    public class CarResponse
+    {
+        public CarDto Car { get; set; }
+    }
+
+    public class CarDto
+    {
+        public int Id { get; set; }
+        public string Car { get; set; }
+        public string Car_Model { get; set; }
+        public string Car_Color { get; set; }
+        public int Car_Model_Year { get; set; }
+        public string Car_Vin { get; set; }
+        public string Price { get; set; }
+        public bool Availability { get; set; }
+    }
+}
