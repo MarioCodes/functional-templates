1567c0246c573a73a33d997a7cb0184859ccc3e5
Mario Codes <mariocodes1993@gmail.com>
2025-11-03 12:41:47 +0100

    remove old templates

--- _outdated/broken/kotlin-springboot-jpa/app/src/main/java/es/msanchez/frameworks/spring/boot/dao/HobbyDao.kt
+++
@@ -1,7 +0,0 @@
-package es.msanchez.frameworks.spring.boot.dao
-
-import es.msanchez.frameworks.spring.boot.entity.Hobby
-import org.springframework.stereotype.Repository
-
-@Repository
-interface HobbyDao : RawDao<Hobby>
